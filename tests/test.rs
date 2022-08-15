b4c84d4f7251a0cd6e6255f7053ec91620a83861
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2022-08-15 18:24:31 +0200

    Reformatted tests for debugability

--- tests/test.rs
+++ tests/test.rs
@@ -3,17 +3,17 @@ mod tests {
     use apex_legends::data_types;
     use std::env;
 
-    fn print_data<T>(res: Result<T, String>, f: fn(T) -> String) {
+    fn print_data<T>(res: Result<T, String>, f: fn(T) -> String) -> bool {
         match res {
             Ok(data) => {
                 println!("{}", f(data));
 
-                assert!(true)
+                true
             }
             Err(e) => {
                 println!("there was an error!: {}", e);
 
-                assert!(false)
+                false
             }
         }
     }
@@ -25,7 +25,7 @@ mod tests {
         let user_name = env::var("USERNAME").expect("Expected key USERNAME");
         let api_key = env::var("API_KEY").expect("Expected key API_KEY");
 
-        print_data::<data_types::ApexUser>(
+        assert!(print_data::<data_types::ApexUser>(
             apex_legends::get_user_retry(String::from(&user_name), &api_key, true).await,
             |data| {
                 format!(
@@ -33,9 +33,9 @@ mod tests {
                     data.global.level, data.stats.br_kills.value
                 )
             },
-        );
+        ), "get_user_retry");
 
-        print_data::<data_types::ApexUser>(
+        assert!(print_data::<data_types::ApexUser>(
             apex_legends::get_user_retry(String::from(&user_name), &api_key, true).await,
             |data| {
                 format!(
@@ -43,26 +43,26 @@ mod tests {
                     data.global.level, data.stats.br_kills.value
                 )
             },
-        );
+        ), "get_user_retry");
     }
     #[tokio::test]
-    #[should_panic] // Because the API returns 200 OK when it should return 429
     async fn uid() {
         dotenv::dotenv().expect("Could not load .env file");
 
         let user_name = env::var("USERNAME").expect("Expected key USERNAME");
         let api_key = env::var("API_KEY").expect("Expected key API_KEY");
 
-        print_data::<data_types::ApexProfile>(
+        assert!(print_data::<data_types::ApexProfile>(
             apex_legends::get_uid_from_username_retry(String::from(&user_name), &api_key, true)
                 .await,
             |data| format!("Your UID is {}", data.uid),
-        );
-        print_data::<data_types::ApexProfile>(
+        ), "get_uid_from_username_retry");
+
+        assert!(print_data::<data_types::ApexProfile>(
             apex_legends::get_uid_from_username_retry(String::from(&user_name), &api_key, true)
                 .await,
             |data| format!("Your UID is {}", data.uid),
-        );
+        ), "get_uid_from_username_retry");
     }
 
     #[tokio::test]
@@ -71,7 +71,7 @@ mod tests {
 
         let api_key = env::var("API_KEY").expect("Expected key API_KEY");
 
-        print_data::<data_types::ApexMapRotation>(
+        assert!(print_data::<data_types::ApexMapRotation>(
             apex_legends::get_map_rotation_retry(&api_key, true).await,
             |data| {
                 format!(
@@ -79,8 +79,8 @@ mod tests {
                     data.arenas_ranked.current.map
                 )
             },
-        );
-        print_data::<data_types::ApexMapRotation>(
+        ), "get_map_rotation_retry");
+        assert!(print_data::<data_types::ApexMapRotation>(
             apex_legends::get_map_rotation_retry(&api_key, true).await,
             |data| {
                 format!(
@@ -88,6 +88,6 @@ mod tests {
                     data.arenas_ranked.current.map
                 )
             },
-        );
+        ), "get_map_rotation_retry");
     }
 }
